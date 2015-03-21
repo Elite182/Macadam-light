d1c1ca9303a1c1383e176ce8f2c8e4f4dace3a4a
Cédric BOULETKESSLER <cedric.bouletkessler@gmail.com>
2015-03-21 15:48:29 +0100

    mock trip new view

--- frontend/src/app/tripsnew/tripsnew.controller.js
+++ frontend/src/app/tripsnew/tripsnew.controller.js
@@ -12,7 +12,18 @@
 
     function TripsNewController($scope, TripsService) {
         
-
+    	$scope.trip = {
+    		_id: 0,
+		    title: "",
+		    description: "",
+		    location: "",
+		    date: "",
+		    time: "",
+		    center: { latitude: 120, longitude: 120
+		    },
+		    zoom: 10,
+		    markers: [ { latitude: 120, longitude: 120 } ]
+    	};
 
         ////////////
 
