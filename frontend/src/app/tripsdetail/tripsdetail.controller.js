4799504095682dc6383f4b4d32c82a1f71528cde
Cédric BOULETKESSLER <cedric.bouletkessler@gmail.com>
2015-03-21 13:36:02 +0100

    mockup of detail view

--- frontend/src/app/tripsdetail/tripsdetail.controller.js
+++ frontend/src/app/tripsdetail/tripsdetail.controller.js
@@ -12,7 +12,31 @@
 
     function TripsDetailController($scope, TripsService) {
         
-
+    	$scope.trip = {
+	        _id: 0,
+	        title: "JCC",
+	        description: "A JCC production !",
+	        location: "Ile-de-France",
+	        date: "2015-03-20",
+	        time: "15:00",
+	        participants: [
+	            "julien",
+	            "clément F",
+	            "cédric",
+	            "clément N"
+	        ],
+	        center: {
+	            latitude: 120,
+	            longitude: 120
+	        },
+	        zoom: 10,
+	        markers: [
+	            {
+	                latitude: 120,
+	                longitude: 120
+	            }
+	        ]
+    	};
 
         ////////////
 
